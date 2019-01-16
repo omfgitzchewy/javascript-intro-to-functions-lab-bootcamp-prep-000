b907606e94e499b43f90d44153a543a3c2eb417d
Aaron Chu <aaronchu357@gmail.com>
2019-01-16 23:03:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string){
+    return "I can't hear you!"
+  } else if (string.toUpperCase() === string){
+    return "YES INDEED!"
+  } else ("I love you, Grandma." === string){
+    return "I love you, too."
+  }
+}
