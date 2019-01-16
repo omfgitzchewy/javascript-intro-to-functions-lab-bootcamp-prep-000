0985efa0a2dba529b4664b03d7bcd4e8226c8afc
Aaron Chu <aaronchu357@gmail.com>
2019-01-16 22:59:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } else if (string.toUpperCase() === string){
     return "YES INDEED!"
-  } else (string === "I love you, Grandma."){
+  } else ("I love you, Grandma." === string){
     return "I love you, too."
   }
 }
